cd0212a709f45ba7aa77ce3a44b76fcf8dd225d8
pRusnak2 <prusnak2@student.umb.sk>
2024-04-02 17:07:27 +0200

    uprava vizualnej stranky formulara

--- src/app/restauracia-formular/restauracia-formular.component.ts
+++ src/app/restauracia-formular/restauracia-formular.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
-import {FormsModule} from "@angular/forms";
-import {RouterOutlet} from "@angular/router";
+import {FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators, FormsModule} from '@angular/forms';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatOptionModule} from "@angular/material/core";
+import {MatIcon} from "@angular/material/icon";
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-restauracia-formular',
   standalone: true,
-  imports: [FormsModule, RouterOutlet],
+  imports: [
+    MatFormFieldModule,
+    MatSelectModule,
+    MatOptionModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatIcon,
+    MatButtonModule,
+    FormsModule
+  ],
   templateUrl: './restauracia-formular.component.html',
   styleUrl: './restauracia-formular.component.css'
 })
