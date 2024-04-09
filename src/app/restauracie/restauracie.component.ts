9abb2f9910978c49bda6a030326055f201f3a747
kristian <kgrzanka@student.umb.sk>
2024-04-09 17:13:02 +0200

    pridanie fukncie na zobrazenie vsetkych restik,zobrazenie vsetkych restik.

--- src/app/restauracie/restauracie.component.ts
+++ src/app/restauracie/restauracie.component.ts
@@ -1,14 +1,35 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormsModule} from "@angular/forms";
 import {RouterOutlet} from "@angular/router";
+import {Restauracia} from "../model/restauracia.model";
+import {RestauraciaService} from "../services/restauracia.service";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-restauracie',
   standalone: true,
-  imports: [FormsModule, RouterOutlet],
+  imports: [FormsModule, RouterOutlet, CommonModule],
   templateUrl: './restauracie.component.html',
   styleUrl: './restauracie.component.css'
 })
-export class RestauracieComponent {
+export class RestauracieComponent implements OnInit {
+  restauracie: Restauracia[] = [];
 
+  constructor(private restauraciaService: RestauraciaService) { }
+
+  ngOnInit(): void {
+    this.ziskajVsetkyRestauracie();
+  }
+
+  ziskajVsetkyRestauracie(): void {
+    this.restauraciaService.ziskajVsetkyObsahyRestauracii().subscribe({
+      next: (restauracie) => {
+        this.restauracie = restauracie;
+      },
+      error: (error) => {
+        console.error('Chyba pri získavaní reštaurácií', error);
+
+      }
+    });
+  }
 }
--- src/app/services/restauracia.service.ts
+++ src/app/services/restauracia.service.ts
@@ -15,4 +15,9 @@ export class RestauraciaService {
     return this.http.post<number>(this.apiUrlRestauracia, restauracia);
   }
 
+  ziskajVsetkyObsahyRestauracii(): Observable<any[]> {
+    const apiUrl = `${this.apiUrlRestauracia}/vsetky`;
+    return this.http.get<any[]>(apiUrl);
+  }
+
 }
