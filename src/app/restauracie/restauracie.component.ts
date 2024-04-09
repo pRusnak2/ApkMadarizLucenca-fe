d150e2198daccc461e8f19442d87d5aad6f87132
pRusnak2 <prusnak2@student.umb.sk>
2024-04-09 19:30:11 +0200

    vzhlad vypisu restauracii

--- src/app/restauracie/restauracie.component.ts
+++ src/app/restauracie/restauracie.component.ts
@@ -4,11 +4,15 @@ import {RouterOutlet} from "@angular/router";
 import {Restauracia} from "../model/restauracia.model";
 import {RestauraciaService} from "../services/restauracia.service";
 import { CommonModule } from '@angular/common';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-restauracie',
   standalone: true,
-  imports: [FormsModule, RouterOutlet, CommonModule],
+  imports: [FormsModule, RouterOutlet, CommonModule, MatProgressBarModule, MatButtonModule, MatDividerModule, MatCardModule],
   templateUrl: './restauracie.component.html',
   styleUrl: './restauracie.component.css'
 })
