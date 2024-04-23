b3a34c6ceae605c324fe2721b978c2d52631cf2f
pRusnak2 <prusnak2@student.umb.sk>
2024-04-23 16:40:25 +0200

    Uvod

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,7 @@ import {RegistraciaComponent} from "./registracia/registracia.component";
 import {RestauraciaFormularComponent} from "./restauracia-formular/restauracia-formular.component";
 import {RestauraciaDetailComponent} from "./restauracia-detail/restauracia-detail.component";
 import {ObjednavkyComponent} from "./objednavky/objednavky.component";
+import {UvodComponent} from "./uvod/uvod.component";
 
 export const routes: Routes = [
   { path: 'restauracie', component: RestauracieComponent },
@@ -14,5 +15,6 @@ export const routes: Routes = [
   { path: 'restauracia-formular', component: RestauraciaFormularComponent },
   { path: 'registracia', component: RegistraciaComponent },
   { path: 'restauracia-detail/:restaurantId', component: RestauraciaDetailComponent },
+  { path: '', component: UvodComponent },
   {path: 'objednavky', component: ObjednavkyComponent}
 ];
---
+++ src/app/uvod/uvod.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-uvod',
+  standalone: true,
+  imports: [],
+  templateUrl: './uvod.component.html',
+  styleUrl: './uvod.component.css'
+})
+export class UvodComponent {
+
+}
