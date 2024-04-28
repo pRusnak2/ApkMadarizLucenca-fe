22a5751ae4951871e1538db25d4d5b88571db6a7
rsojka <rsojka@student.umb.sk>
2024-04-28 19:26:31 +0200

    nav, footer, style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet, RouterModule } from '@angular/router';
 import {MatIconModule} from '@angular/material/icon';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatButtonModule} from '@angular/material/button';
+import {NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterModule, MatIconModule, RouterModule, MatMenuModule, MatButtonModule],
+  imports: [RouterOutlet, RouterModule, MatIconModule, RouterModule, MatMenuModule, MatButtonModule, NgOptimizedImage],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
