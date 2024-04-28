807d0d6eafc468c798f3ac24a5b5d367a3f44120
lidea <rsojka@student.umb.sk>
2024-04-28 23:02:22 +0200

    footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,6 +9,9 @@ import {ObjednavkyComponent} from "./objednavky/objednavky.component";
 import {UvodComponent} from "./uvod/uvod.component";
 import {JedloFormularComponent} from "./jedlo-formular/jedlo-formular.component";
 import {KosikComponent} from "./kosik/kosik.component";
+import {CastoKladeneOtazkyComponent} from "./footer content/casto-kladene-otazky/casto-kladene-otazky.component";
+import {ONasComponent} from "./footer content/o-nas/o-nas.component";
+
 
 export const routes: Routes = [
   { path: 'restauracie', component: RestauracieComponent },
@@ -20,5 +23,7 @@ export const routes: Routes = [
   { path: '', component: UvodComponent },
   {path: 'objednavky', component: ObjednavkyComponent},
   {path: 'kosik', component: KosikComponent},
-  {path: 'jedlo-formular', component: JedloFormularComponent}
+  {path: 'jedlo-formular', component: JedloFormularComponent},
+  {path: 'casto-kladene-otazky', component: CastoKladeneOtazkyComponent},
+  {path: 'o-nas', component: ONasComponent},
 ];
