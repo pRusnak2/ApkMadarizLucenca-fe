bc61902e7ce053d78a9169d153cc4edeb5ba4053
Matus Stankovic <xstankovic@stuba.sk>
2024-05-22 22:47:35 +0200

    Zobrazenie objednávok pre zákazníka

--- src/app/services/objednavky.service.ts
+++ src/app/services/objednavky.service.ts
@@ -1,16 +1,17 @@
-import {HttpClient} from "@angular/common/http";
-import {Observable} from "rxjs";
-import {Objednavky} from "../model/objednavky.model";
-import {Injectable} from "@angular/core";
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { Order } from '../model/order.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ObjednavkyService {
 
-  private apiUrlOrderDetail: string = "";
+  private apiUrlOrderDetail: string = 'http://localhost:8080/orders';
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) {
+  getOrders(): Observable<Order[]> {
+    return this.http.get<Order[]>(this.apiUrlOrderDetail);
   }
-
 }
